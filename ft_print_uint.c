3423c2f5035c4a8b4e94fad9f7935bbb69c7a5f0
Diogo Marcelo De Paulo Triles <dde-paul@a2-r3-p4.s19.be>
2025-11-14 17:34:05 +0100

    edit ft_print_uint

--- ft_print_uint.c
+++ ft_print_uint.c
@@ -6,7 +6,7 @@
 /*   By: dde-paul <marvin@42.fr>                    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/11/10 20:22:40 by dde-paul          #+#    #+#             */
-/*   Updated: 2025/11/13 00:38:11 by dde-paul         ###   ########.fr       */
+/*   Updated: 2025/11/14 17:33:54 by dde-paul         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -17,37 +17,17 @@ static void	ft_putchar(int c)
 	write(1, &c, 1);
 }
 
-static void	ft_putnbr(unsigned long nb)
-{
-	if (nb >= 10)
-	{
-		ft_putnbr(nb / 10);
-	}
-	ft_putchar((nb % 10) + '0');
-}
-
-static unsigned int	ft_uintlen(unsigned int n)
-{
-	unsigned int	i;
-
-	if (n == 0)
-		return (1);
-	i = 0;
-	while (n > 0)
-	{
-		n = n / 10;
-		i++;
-	}
-	return (i);
-}
-
 unsigned int	ft_print_uint(unsigned int nb)
 {
-	unsigned int	len;
-	unsigned long	n;
+	int	i;
+	unsigned long long	n;
+	int	count;
 
+	i = 0;
 	n = nb;
-	len = ft_uintlen(n);
-	ft_putnbr(n);
-	return (len);
+	if (n > 9)
+		count += ft_print_uint(n / 10);
+	count++;
+	ft_putchar(nb + '0');
+	return (count);
 }
